d7cea81c0065ec09851094b3243d45af4b0683d5
ghkdwnsdlr <imbanana15@gmail.com>
2025-02-10 21:49:40 +0900

    1

--- src/main/java/com/springstudy/docker/BasicController.java
+++ src/main/java/com/springstudy/docker/BasicController.java
@@ -1,4 +1,15 @@
 package com.springstudy.docker;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class BasicController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String post() {
+        return "hello";
+    }
 }
